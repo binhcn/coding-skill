48d304ea11f0d91ff5741e7d0e21da226c98dae4
binhcaong <binhcn@vng.com.vn>
2022-05-11 02:29:26 +0700

    update code

--- src/main/java/dev/binhcn/MedianOfTwoSortedArrays.java
+++ src/main/java/dev/binhcn/MedianOfTwoSortedArrays.java
@@ -22,24 +22,19 @@ public class MedianOfTwoSortedArrays {
     }
     int m = nums1.length, n = nums2.length, start = 0, end = m;
     while (start <= end) {
-      int partitionNums1 = (start + end) / 2;
-      int partitionNums2 = (m + n + 1) / 2 - partitionNums1;
-      int maxLeftNums1 = partitionNums1 == 0 ? Integer.MIN_VALUE : nums1[partitionNums1 - 1];
-      int minRightNums1 = partitionNums1 == m ? Integer.MAX_VALUE : nums1[partitionNums1];
-      int maxLeftNums2 = partitionNums2 == 0 ? Integer.MIN_VALUE : nums2[partitionNums2 - 1];
-      int minRightNums2 = partitionNums2 == n ? Integer.MAX_VALUE : nums2[partitionNums2];
+      int partition1 = (start + end) / 2;
+      int partition2 = (m + n + 1) / 2 - partition1;
+      int maxLeftNums1 = partition1 == 0 ? Integer.MIN_VALUE : nums1[partition1 - 1];
+      int minRightNums1 = partition1 == m ? Integer.MAX_VALUE : nums1[partition1];
+      int maxLeftNums2 = partition2 == 0 ? Integer.MIN_VALUE : nums2[partition2 - 1];
+      int minRightNums2 = partition2 == n ? Integer.MAX_VALUE : nums2[partition2];
 
-      if (maxLeftNums1 <= minRightNums2 && maxLeftNums2 <= minRightNums1) {
-        if ((m + n) % 2 == 0) {
-          return (Math.max(maxLeftNums1, maxLeftNums2) + Math.min(minRightNums1, minRightNums2)) / 2.0;
-        } else {
-          return Math.max(maxLeftNums1, maxLeftNums2);
-        }
-      } else if (maxLeftNums1 > minRightNums2) {
-        end = partitionNums1 - 1;
-      } else {
-        start = partitionNums1 + 1;
-      }
+      if (maxLeftNums1 < minRightNums2 && maxLeftNums2 < minRightNums1) {
+        return (n + m) % 2 == 0
+            ? (Math.max(maxLeftNums1, maxLeftNums2) + Math.min(minRightNums1, minRightNums2)) / 2.0
+            : Math.max(maxLeftNums1, maxLeftNums2);
+      } else if (maxLeftNums1 > minRightNums2) end = partition1 - 1;
+      else start = partition1 + 1;
     }
     throw new IllegalArgumentException();
   }
